65062016dd999a85a60b33abb4fa4d1fe4fbc808
seid8974 <seid897474@gmail.com>
2026-01-25 21:47:30 +0300

    Update WeatherProject backend and frontend components

--- BACKEND/Angela-yu-node-js/WeatherProject/app.js
+++ BACKEND/Angela-yu-node-js/WeatherProject/app.js
@@ -1,31 +1,49 @@
 const express = require('express');
 const https = require('https');
+const bodyParser = require("body-parser");
+const port = 3000;
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 app.get("/",(req,res) =>{
 
+ res.sendFile(__dirname + "/public/index.html");
 
-   const url = "https://official-joke-api.appspot.com/random_joke";
-
-
-
-  https.get(url,(response) => {
-        console.log(response.statusCode);
-
-       response.on("data",(data) => {
-        const weatherData = JSON.parse(data);
-        const id = weatherData.id;
-        const type = weatherData.type;
-
-       })
-
-    })
-
-
-    res.send("the server is up and running");
 })
 
-app.listen(3000,(req,res) => {
+app.post("/",(req,res) => {
+
+    const cityName = req.body.city;
+    const apikey = "8e799d72102d302f6c12330e600bde24";
+    const units = "metric";
+
+   const url = 'https://api.openweathermap.org/data/2.5/weather?q='+ cityName +'&units='+ units +'&appid='+ apikey;
+
+  https.get(url,(response) => {
+    
+       response.on("data",(data) => {
+
+            const weatherData = JSON.parse(data);
+
+            const city = weatherData.name;
+            const weatherTemp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageUrl = 'https://openweathermap.org/img/wn/' + icon + '@2x.png'
+
+      res.json({
+        city: city,
+        temp: weatherTemp,
+        condition: weatherDescription,
+        image: imageUrl
+      });
+       })
+    })
+})
+
+app.listen(port,(req,res) => {
+    console.log('the port is http://localhost:'+ port);
     console.log("the server is running on port 3000");
 })
